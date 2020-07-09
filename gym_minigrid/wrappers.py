9024ab1fdbf132e4022906c1f996a1b503d4955f
juan <jbarsce@gmail.com>
2020-07-09 18:56:16 -0300

    added fullyobs no-dict wraper

--- gym_minigrid/wrappers.py
+++ gym_minigrid/wrappers.py
@@ -255,6 +255,33 @@ class FullyObsWrapper(gym.core.ObservationWrapper):
         }
 
 
+class FullyObsWrapperNoMissionDict(gym.core.ObservationWrapper):
+    """
+    Fully observable gridworld using a compact grid encoding
+    """
+
+    def __init__(self, env):
+        super().__init__(env)
+
+        self.observation_space = spaces.Box(
+            low=0,
+            high=255,
+            shape=(self.env.width, self.env.height, 3),  # number of cells
+            dtype='uint8'
+        )
+
+    def observation(self, obs):
+        env = self.unwrapped
+        full_grid = env.grid.encode()
+        full_grid[env.agent_pos[0]][env.agent_pos[1]] = np.array([
+            OBJECT_TO_IDX['agent'],
+            COLOR_TO_IDX['red'],
+            env.agent_dir
+        ])
+
+        return full_grid
+
+
 class FlatObsWrapper(gym.core.ObservationWrapper):
     """
     Encode mission strings using a one-hot scheme,
